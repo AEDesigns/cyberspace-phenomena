b31fe7865d64a0ce70d65b299d82a8230078a7ac
Andrew Elliott <mtgelliott15@gmail.com>
2020-01-23 14:55:19 -0600

    Add Carousel Feature in ModalComponents

--- src/Components/ModalComponents/FirstProject.js
+++ src/Components/ModalComponents/FirstProject.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
+import Carousel from "react-bootstrap/Carousel";
 
 const FirstProjectModal = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -18,10 +19,55 @@ const FirstProjectModal = () => {
         Views of Lathropinsurance.com
       </button>
       <Modal show={isOpen} onHide={hideModal}>
-        <Modal.Header>
-          <Modal.Title>Hi</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>The body</Modal.Body>
+        <Modal.Body>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../Imgs/Lathrop-logo.png")}
+                width="250"
+                height="100"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../Imgs/Lathrop-logo.png")}
+                width="2500"
+                height="100"
+                alt="First slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../Imgs/Lathrop-logo.png")}
+                width="250"
+                height="100"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Modal.Body>
         <Modal.Footer>
           <button variant="primary" onClick={hideModal}>
             Close
